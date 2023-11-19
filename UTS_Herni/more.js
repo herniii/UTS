79779a9aa2d5dd7e84a0a8e5272ccab68dafc0ac
herniii <145246517+herniii@users.noreply.github.com>
2023-11-19 21:50:03 +0700

    Add files via upload

---
+++ UTS_Herni/more.js
@@ -0,0 +1,71 @@
+//Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+//mengaktifkan hamburger-menu
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//close hamburger-menu di luar icon
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+let slide = document.querySelectorAll('.slide')
+var current = 0;
+
+function cls(){
+    for(let i = 0; i < slide.length; i++){
+        slide[i].style.display = 'none';
+    }
+}
+
+function next(){
+    cls();
+    if(current === slide.length-1) current = -1;
+    current++;
+
+    slide[current].style.display = 'block';
+    slide[current].style.opacity = 0.4;
+
+    var x = 0.4;
+    var intX = setInterval(function(){
+        x += 0.1;
+        slide[current].style.opacity = x;
+        if(x >= 1){
+            clearInterval(intX);
+            x = 0.4;
+        }
+    }, 100);
+}
+
+function prev(){
+    cls();
+    if(current === 0) current = slide.length;
+    current--;
+
+    slide[current].style.display = 'block';
+    slide[current].style.opacity = 0.4;
+
+    var x = 0.4;
+    var intX = setInterval(function() {
+        x+=0.1;
+        slide[current].style.opacity = x;
+        if(x >= 1){
+            clearInterval(intX);
+            x = 0.4
+        }
+        
+    }, 100);
+}
+
+function start(){
+    cls();
+    slide[current].style.display = 'block';
+}
+start();
